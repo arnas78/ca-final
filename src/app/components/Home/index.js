d0385812c1ed876ed20954c26bb9605a57195c0f
Arnas Klimašauskas <arnasklimas@gmail.com>
2023-02-11 18:58:43 +0200

    input addition

--- src/app/components/Home/index.js
+++ src/app/components/Home/index.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
 
 function Header() {
+  const options = [
+    {value: 'apple', text: 'Vyras'},
+    {value: 'banana', text: 'Moteris'},
+    {value: 'kiwi', text: 'Kita'},
+  ];
+
+  const [startDate, setStartDate] = useState(new Date());
+  const [number, setNumber] = useState("");
+  const [selected, setSelected] = useState(options[0].value);
+
+  const handleChange = event => {
+    setSelected(event.target.value);
+  };
+
   return (
     <main>
       <section className="Section__intro">
@@ -11,6 +28,20 @@ function Header() {
           pietus, užsiregistruoti apmokymams bei pateikti prašymą į dalyvavimą
           renginyje!
         </p>
+        <div>
+          <label>Vardas:</label>
+          <input type="text" className="Input__profile" placeholder="Arnas" disabled></input>
+          <input type="text" className="Input__profile" placeholder="Klimasauskas" disabled></input>
+          <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+          <select value={selected} onChange={(event) => setSelected(event.target.value)}>
+            {options.map(option => (
+              <option key={option.value} value={option.value}>
+                {option.text}
+              </option>
+            ))}
+          </select>
+          <input type="text" className="Input__profile" onChange={(date) => setStartDate(date)} placeholder="Telefono nr."></input>
+        </div>
       </section>
     </main>
   );
